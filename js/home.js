80ce7f9ed2c80ae75891eccfdb500fdaeef5d425
Vaibhav Desai <vaidesai@ebay.com>
2016-01-27 11:19:05 -0800

    responsive footer

--- js/home.js
+++ js/home.js
@@ -9,9 +9,9 @@ $(window).scroll(function () {
 
 $(function () {
     $(".page-scroll a").bind("click", function () {
-        var $anchor = $(this);
+        var anchor = $(this);
         $("html, body").stop().animate({
-            scrollTop: $($anchor.attr("href").offset().top)
+            scrollTop: $(anchor.attr("href")).offset().top
         }, 1500, "easeInOutExpo");
         event.preventDefault();
     })
